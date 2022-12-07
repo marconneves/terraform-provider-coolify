cdc6dfb68d7b532b9216b53c4beb11f237017184
Mark Will <me@themark.dev>
2022-12-07 01:05:13 -0300

    fix 🐞: Other test for client

--- api/client/client.go
+++ api/client/client.go
@@ -28,29 +28,50 @@ func NewClient(hostname string, token string) *Client {
 
 
 func (client *Client) httpRequest(path, method string, body bytes.Buffer) (closer io.ReadCloser, err error) {
-	f, err := os.Create("test.txt")
-    if err != nil {
-        fmt.Println(err)
-        return
-    }
-	token := fmt.Sprintf("Bearer %v", client.authToken)
-    f.WriteString(token)
-	f.Close()
-
-
 	req, err := http.NewRequest(method, client.requestPath(path), &body)
 	if err != nil {
 		return nil, err
 	}
 
 
-	req.Header.Add("Authorization", token)
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %v", client.authToken))
 	switch method {
 	case "GET":
 	case "DELETE":
 	default:
 		req.Header.Add("Content-Type", "application/json")
 	}
+	f, err := os.Create("a.txt")
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+	f.WriteString(req.Header.Get("Content-Type"))
+	f.Close()
+	
+	f, err = os.Create("b.txt")
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+	f.WriteString(req.Header.Get("Authorization"))
+	f.Close()
+	
+	f, err = os.Create("c.txt")
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+	f.WriteString(req.URL.String())
+	f.Close()
+	
+	f, err = os.Create("d.txt")
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+	f.WriteString(client.requestPath(path))
+	f.Close()
 
 	resp, err := client.httpClient.Do(req)
 	if err != nil {
