9baf100c52971bb371246851d2d393847b08837c
Mark Will <me@themark.dev>
2022-12-23 22:49:17 -0300

    feat ✨: Finish implement base of schema and base of creation application

--- example/application-database-with-destination/main.tf
+++ example/application-database-with-destination/main.tf
@@ -2,14 +2,14 @@ terraform {
   required_providers {
     coolify = {
       source  = "themarkwill/coolify"
-      version = "0.3.0-beta.2"
+      version = "0.3.0-beta.4"
     }
   }
 }
 
 provider "coolify" {
-  address = "https://services.b4.run"
-  token   = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJjbGJmb2JuOHEwMGFoOWRtbzgxY2k5bHl4IiwidGVhbUlkIjoiY2xiOXZrbGp3MDAwdm1vOWRlbHFyNjFtaSIsInBlcm1pc3Npb24iOiJhZG1pbiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY3MDU0NDI4OX0.mc08PJ0b0JDQbU4Gt-uOVrawL5TyMewwbGD1jQperl0"
+  address = "url of coolify"
+  token = "token"
 }
 
 resource "coolify_destination" "network" {
@@ -17,7 +17,7 @@ resource "coolify_destination" "network" {
   network = "second-network-bibi"
 }
 
-resource "coolify_database" "smy_db" {
+resource "coolify_database" "redis" {
   name   = "second-application-db"
   engine = "redis:7.0"
 
@@ -49,8 +49,18 @@ resource "coolify_application" "test_item" {
     }
 
     env {
-      key   = "BASE_URL"
-      value = "https://front.s.coolify.io"
+      key   = "REDIS_PASSWORD"
+      value = coolify_database.redis.status.password
+    }
+
+    env {
+      key   = "REDIS_HOST"
+      value = coolify_database.redis.status.host
+    }
+
+    env {
+      key   = "REDIS_PORT"
+      value = coolify_database.redis.status.port
     }
   }
 
