5a39688556b1f842af8bfb345d2444ef331effdf
Mark Will <me@themark.dev>
2022-12-25 21:59:56 -0300

    feat ✨: Setting to use base of test from utils

--- coolify/application/resource_test.go
+++ coolify/application/resource_test.go
@@ -2,15 +2,14 @@ package application_test
 
 import (
 	"fmt"
-	"regexp"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	tf "github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 
 	"terraform-provider-coolify/api/client"
 	"terraform-provider-coolify/coolify"
+	"terraform-provider-coolify/shared/tests"
 )
 
 var TestAccProviders map[string]*schema.Provider
@@ -23,16 +22,27 @@ func init() {
 	}
 }
 
+func GetApplication(client *client.Client, id string) (interface{}, error) {
+	return client.GetApplication(id)
+}
+
 func TestAccApplication_Basic(t *testing.T) {
 	resource.Test(t, resource.TestCase{
-		PreCheck:     func() {},
-		Providers:    TestAccProviders,
-		CheckDestroy: testAccCheckItemDestroy,
+		PreCheck:  func() {},
+		Providers: TestAccProviders,
+		CheckDestroy: tests.TestAccCheckDestroy(
+			TestAccProvider,
+			GetApplication,
+		),
 		Steps: []resource.TestStep{
 			{
 				Config: testAccCheckItemBasic(),
 				Check: resource.ComposeTestCheckFunc(
-					testAccCheckExampleItemExists("coolify_application.test_item"),
+					tests.TestAccCheckExists(
+						"coolify_application.test_item",
+						TestAccProvider,
+						GetApplication,
+					),
 					resource.TestCheckResourceAttr(
 						"coolify_application.test_item", "name", "first-app"),
 					resource.TestCheckResourceAttrSet(
@@ -41,47 +51,6 @@ func TestAccApplication_Basic(t *testing.T) {
 			},
 		},
 	})
-}
-
-func testAccCheckItemDestroy(s *tf.State) error {
-	apiClient := TestAccProvider.Meta().(*client.Client)
-
-	for _, rs := range s.RootModule().Resources {
-		if rs.Type != "example_item" {
-			continue
-		}
-
-		_, err := apiClient.GetApplication(rs.Primary.ID)
-		if err == nil {
-			return fmt.Errorf("Alert still exists")
-		}
-		notFoundErr := "not found"
-		expectedErr := regexp.MustCompile(notFoundErr)
-		if !expectedErr.Match([]byte(err.Error())) {
-			return fmt.Errorf("expected %s, got %s", notFoundErr, err)
-		}
-	}
-
-	return nil
-}
-
-func testAccCheckExampleItemExists(resource string) resource.TestCheckFunc {
-	return func(state *tf.State) error {
-		rs, ok := state.RootModule().Resources[resource]
-		if !ok {
-			return fmt.Errorf("Not found: %s", resource)
-		}
-		if rs.Primary.ID == "" {
-			return fmt.Errorf("No Record ID is set")
-		}
-		id := rs.Primary.ID
-		apiClient := TestAccProvider.Meta().(*client.Client)
-		_, err := apiClient.GetApplication(id)
-		if err != nil {
-			return fmt.Errorf("error fetching item with resource %s. %s", resource, err)
-		}
-		return nil
-	}
 }
 
 func testAccCheckItemBasic() string {
--- coolify/destination/data_source_test.go
+++ coolify/destination/data_source_test.go
@@ -3,20 +3,29 @@ package destination_test
 import (
 	"testing"
 
+	"terraform-provider-coolify/shared/tests"
+
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
 func TestAccDestination_DataSource(t *testing.T) {
 	resource.Test(t, resource.TestCase{
-		PreCheck:     func() {},
-		Providers:    TestAccProviders,
-		CheckDestroy: testAccCheckItemDestroy,
+		PreCheck:  func() {},
+		Providers: TestAccProviders,
+		CheckDestroy: tests.TestAccCheckDestroy(
+			TestAccProvider,
+			GetDestination,
+		),
 		Steps: []resource.TestStep{
 			{
 				Config: testAccDataSourceSpannerInstanceBasic(),
 				Check: resource.ComposeTestCheckFunc(
-					testAccCheckExampleItemExists("data.coolify_destination.network"),
-					checkAttribute("data.coolify_destination.network", "name", "my-network"),
+					tests.TestAccCheckExists(
+						"data.coolify_destination.network",
+						TestAccProvider,
+						GetDestination,
+					),
+					tests.CheckAttribute("data.coolify_destination.network", "name", "my-network"),
 				),
 			},
 		},
