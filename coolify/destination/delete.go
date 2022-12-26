4880031f3a8f049e433b826d07188d48562a601e
Mark Will <me@themark.dev>
2022-12-25 21:59:16 -0300

    feat ✨: Add delay on destroy destination

--- coolify/destination/delete.go
+++ coolify/destination/delete.go
@@ -3,6 +3,7 @@ package destination
 import (
 	"context"
 	"terraform-provider-coolify/api/client"
+	"time"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
@@ -11,6 +12,9 @@ import (
 func destinationDeleteItem(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	apiClient := m.(*client.Client)
 	destinationId := d.Id()
+
+	// For finish exclude all items used on destination
+	time.Sleep(time.Duration(30) * time.Second)
 
 	err := apiClient.DeleteDestination(destinationId)
 	if err != nil {
