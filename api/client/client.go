40f7a96afdf2bea5841baf25720ab0d53e3cde40
Marcon Neves <marcon@neves.run>
2024-07-26 08:19:45 -0300

    feat: add enable and disable api

--- api/client/client.go
+++ api/client/client.go
@@ -24,22 +24,35 @@ func NewClient(hostname string, apiToken string) *Client {
 	}
 }
 
-func (client *Client) httpRequest(path, method string, body bytes.Buffer) (closer io.ReadCloser, err error) {
+func (client *Client) httpRequest(path, method string, body ...bytes.Buffer) (closer io.ReadCloser, err error) {
 	url := client.requestPath(path)
+	var bodyBuffer bytes.Buffer
 
-	req, err := http.NewRequest(method, url, &body)
+	if len(body) > 0 {
+		bodyBuffer = body[0]
+	}
+
+	req, err := http.NewRequest(method, url, &bodyBuffer)
 	if err != nil {
 		return nil, err
 	}
 
 	req.Header.Add("Authorization", "Bearer "+client.apiToken)
-	if body.Len() > 0 {
+	if bodyBuffer.Len() > 0 {
 		req.Header.Add("Content-Type", "application/json")
 	}
 
 	resp, err := client.httpClient.Do(req)
 	if err != nil {
 		return nil, err
+	}
+
+	if resp.StatusCode == http.StatusUnauthorized {
+		return nil, fmt.Errorf("unauthenticated")
+	}
+
+	if resp.StatusCode == http.StatusBadRequest {
+		return nil, fmt.Errorf("invalid token")
 	}
 
 	if resp.StatusCode != http.StatusOK && resp.StatusCode != http.StatusCreated {
