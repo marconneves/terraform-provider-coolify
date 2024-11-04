3edb5d1289b56b5fb6a1ab345ba6060160aa01cf
Marcon Neves <marcon@neves.run>
2024-11-04 06:29:04 -0300

    fix: re start from template base

--- main.go
+++ main.go
@@ -1,23 +1,43 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
 package main
 
-// https://github.com/spaceapegames/terraform-provider-example/blob/master/provider/resource_item.go
-// https://github.com/hashicorp/terraform-provider-hashicups
-// https://medium.com/spaceapetech/creating-a-terraform-provider-part-1-ed12884e06d7
-// https://medium.com/rungo/everything-you-need-to-know-about-packages-in-go-b8bac62b74cc
-// Testing
-// Implement Test: https://ieftimov.com/posts/testing-in-go-go-test/
-
 import (
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
+	"context"
+	"flag"
+	"log"
 
-	"terraform-provider-coolify/coolify"
+	"github.com/hashicorp/terraform-plugin-framework/providerserver"
+	"github.com/hashicorp/terraform-provider-scaffolding-framework/internal/provider"
+)
+
+var (
+	// these will be set by the goreleaser configuration
+	// to appropriate values for the compiled binary.
+	version string = "dev"
+
+	// goreleaser can pass other information to the main package, such as the specific commit
+	// https://goreleaser.com/cookbooks/using-main.version/
 )
 
 func main() {
-	plugin.Serve(&plugin.ServeOpts{
-		ProviderFunc: func() *schema.Provider {
-			return coolify.Provider()
-		},
-	})
+	var debug bool
+
+	flag.BoolVar(&debug, "debug", false, "set to true to run the provider with support for debuggers like delve")
+	flag.Parse()
+
+	opts := providerserver.ServeOpts{
+		// TODO: Update this string with the published name of your provider.
+		// Also update the tfplugindocs generate command to either remove the
+		// -provider-name flag or set its value to the updated provider name.
+		Address: "registry.terraform.io/hashicorp/scaffolding",
+		Debug:   debug,
+	}
+
+	err := providerserver.Serve(context.Background(), provider.New(version), opts)
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
 }
