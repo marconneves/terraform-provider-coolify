9d1f1ac606ff171f26cff8d8aec124b90cca4c70
Marcon Neves <marcon@neves.run>
2024-11-04 06:58:05 -0300

    fix: update base of coolify

--- main.go
+++ main.go
@@ -1,6 +1,3 @@
-// Copyright (c) HashiCorp, Inc.
-// SPDX-License-Identifier: MPL-2.0
-
 package main
 
 import (
@@ -28,10 +25,7 @@ func main() {
 	flag.Parse()
 
 	opts := providerserver.ServeOpts{
-		// TODO: Update this string with the published name of your provider.
-		// Also update the tfplugindocs generate command to either remove the
-		// -provider-name flag or set its value to the updated provider name.
-		Address: "registry.terraform.io/hashicorp/scaffolding",
+		Address: "registry.terraform.io/marconneves/coolify",
 		Debug:   debug,
 	}
 
