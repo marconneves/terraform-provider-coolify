ac6b0b85d01899a2c96099b2587ab8709042bc88
Marcon Neves <marcon@neves.run>
2024-11-07 09:56:00 -0300

    feat: update base of project

--- examples/resources/coolify_server/resource.tf
+++ examples/resources/coolify_server/resource.tf
@@ -1,18 +1,3 @@
-
-terraform {
-  required_providers {
-    coolify = {
-      source = "marconneves/coolify"
-      version = "4.4.0-beta.1"
-    }
-  }
-}
-
-provider "coolify" {
-  address = "https://cloud.ferramenta.digital"
-  token = "3|Tt6iybaozpfPZojdlK5gGGotoxK8Cn1u1xTUCe2T1e65744d"
-}
-
 resource "coolify_server" "test" {
     name             = "example-server"
     ip               = "192.168.1.100"
